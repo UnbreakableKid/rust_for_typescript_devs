74c5597ac3c07863898e84fb133358b045237f81
jrp <jrpacheco.dev@gmail.com>
2023-05-11 19:11:49 +0100

    feat: done after borrow

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,1 @@
-fn main() {
-    let filename = std::env::args()
-        .nth(1)
-        .expect("the file name must be passed");
-
-    let file = std::fs::read_to_string(filename).expect("unable to read the file to string");
-
-    file.lines().for_each(|f| {
-        if let Ok(x) = f.parse::<usize>() {
-            println!("{}", x);
-        } else {
-            println!("Line not a number")
-        }
-    })
-}
+fn main() {}
