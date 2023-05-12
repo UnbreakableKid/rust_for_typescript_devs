2275718a0cc7ef1efd397f0c196554a6b57046e4
jrp <jrpacheco.dev@gmail.com>
2023-05-12 16:24:07 +0100

    feat: finished course

--- src/shapes/circle.rs
+++ src/shapes/circle.rs
@@ -1,6 +1,9 @@
-use std::f64::consts::PI;
+use std::{f64::consts::PI, fmt::Display, str::FromStr};
 
-use super::{area::Area, collisions::Collidable, rect::Rect};
+use super::{
+    area::Area,
+    collisions::{Contains, Points},
+};
 
 pub struct Circle {
     pub x: f64,
@@ -8,33 +11,44 @@ pub struct Circle {
     pub radius: f64,
 }
 
-impl Circle {
-    pub fn contains_point(&self, (x, y): (f64, f64)) -> bool {
+impl Contains for Circle {
+    fn contains_point(&self, (x, y): (f64, f64)) -> bool {
         let dx = self.x - x;
         let dy = self.y - y;
         return dx * dx + dy * dy <= self.radius * self.radius;
     }
 }
 
-impl Collidable<Rect> for Circle {
-    fn collide(&self, other: &Rect) -> bool {
-        for point in other {
-            if self.contains_point(point) {
-                return true;
-            }
-        }
-        return false;
-    }
-}
-
-impl Collidable<Circle> for Circle {
-    fn collide(&self, other: &Circle) -> bool {
-        return self.contains_point((other.x, other.y));
+impl Points for Circle {
+    fn points(&self) -> super::collisions::PointIter {
+        return vec![(self.x, self.y)].into();
     }
 }
 
 impl Area for Circle {
     fn area(&self) -> f64 {
         return self.radius * self.radius * PI;
+    }
+}
+
+impl Display for Circle {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        return write!(f, "Circle({}, {}): {}", self.x, self.y, self.radius);
+    }
+}
+
+impl FromStr for Circle {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = s.split(" ").collect::<Vec<_>>();
+        if parts.len() != 3 {
+            return Err(anyhow::anyhow!("bad rectangle"));
+        }
+        return Ok(Circle {
+            x: parts[0].parse()?,
+            y: parts[1].parse()?,
+            radius: parts[2].parse()?,
+        });
     }
 }
--- src/shapes/rect.rs
+++ src/shapes/rect.rs
@@ -1,6 +1,9 @@
-use std::fmt::Display;
+use std::{fmt::Display, str::FromStr};
 
-use super::{area::Area, circle::Circle, collisions::Collidable};
+use super::{
+    area::Area,
+    collisions::{Contains, Points},
+};
 
 pub struct Rect {
     pub x: f64,
@@ -9,26 +12,21 @@ pub struct Rect {
     pub height: f64,
 }
 
-impl Rect {
-    pub fn contains_point(&self, (x, y): (f64, f64)) -> bool {
+impl Contains for Rect {
+    fn contains_point(&self, (x, y): (f64, f64)) -> bool {
         return self.x <= x && self.x + self.width >= x && self.y <= y && self.y + self.height >= y;
     }
 }
 
-impl Collidable<Rect> for Rect {
-    fn collide(&self, other: &Rect) -> bool {
-        for point in other {
-            if self.contains_point(point) {
-                return true;
-            }
-        }
-        return false;
-    }
-}
-
-impl Collidable<Circle> for Rect {
-    fn collide(&self, other: &Circle) -> bool {
-        return self.contains_point((other.x, other.y));
+impl Points for Rect {
+    fn points(&self) -> super::collisions::PointIter {
+        return vec![
+            (self.x, self.y),
+            (self.x + self.width, self.y),
+            (self.x, self.y + self.height),
+            (self.x + self.width, self.y + self.height),
+        ]
+        .into();
     }
 }
 
@@ -59,54 +57,19 @@ impl Display for Rect {
     }
 }
 
-pub struct RectIter {
-    points: Vec<(f64, f64)>,
-    idx: usize,
-}
+impl FromStr for Rect {
+    type Err = anyhow::Error;
 
-impl Iterator for RectIter {
-    type Item = (f64, f64);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let idx = self.idx;
-        self.idx += 1;
-
-        return self.points.get(idx).map(|x| *x);
-    }
-}
-
-impl IntoIterator for Rect {
-    type Item = (f64, f64);
-
-    type IntoIter = RectIter;
-
-    fn into_iter(self) -> Self::IntoIter {
-        return RectIter {
-            points: vec![
-                (self.x, self.y),
-                (self.x + self.width, self.y),
-                (self.x, self.y + self.height),
-                (self.x + self.width, self.y + self.height),
-            ],
-            idx: 0,
-        };
-    }
-}
-
-impl IntoIterator for &Rect {
-    type Item = (f64, f64);
-
-    type IntoIter = RectIter;
-
-    fn into_iter(self) -> Self::IntoIter {
-        return RectIter {
-            points: vec![
-                (self.x, self.y),
-                (self.x + self.width, self.y),
-                (self.x, self.y + self.height),
-                (self.x + self.width, self.y + self.height),
-            ],
-            idx: 0,
-        };
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = s.split(" ").collect::<Vec<_>>();
+        if parts.len() != 4 {
+            return Err(anyhow::anyhow!("bad rectangle"));
+        }
+        return Ok(Rect {
+            x: parts[0].parse()?,
+            y: parts[1].parse()?,
+            width: parts[2].parse()?,
+            height: parts[3].parse()?,
+        });
     }
 }
